acc9f6f19470a527ef3146473539ec28307d975e
ekaterinailin <eilin@aip.de>
2020-02-04 14:46:39 +0100

    Main script will not overwrite a MCMC chain that already started

--- analysis/notebooks/main.py
+++ analysis/notebooks/main.py
@@ -20,20 +20,29 @@ log_probs = {"log_probability":[6, log_probability],
              "log_probability_2flares2ars":[10, log_probability_2flares2ars],}
 
 CWD = "/".join(os.getcwd().split("/")[:-2])
+nwalkers = 32
 
-lcs = {"100004076":{"04_02_2020_12_01":pd.read_csv(f"{CWD}/data/lcs/04_02_2020_12_01_100004076.csv")}}
-
-
-def run_mcmc(ID, tstamp, Nsteps=50000, wiggle=1e-3, nwalkers=32):
-
+def get_inits(ID, tstamp):
+    
     inits = pd.read_csv(f"{CWD}/data/summary/inits.csv")
     target = inits.loc[(inits.ID == ID) & (inits.tstamp==tstamp),:].iloc[0]
     ndim = int(target.nparam)
     
-    lc = lcs[ID][tstamp]
+    assert ndim == log_probs[target.log_prob][0]
+    
+    lc = pd.read_csv(f"{CWD}/data/lcs/{tstamp}_{ID}.csv")
     phi = lc.phi.values
     flux = lc.flux.values
     flux_err = lc.flux_err.values
+    
+    qlum = target.qlum_erg_s * u.erg / u.s
+    Fth = target.Fth * u.erg / u.s / (u.cm)**2
+    
+    return ndim, target, phi, flux, flux_err, qlum, Fth
+    
+def run_mcmc(ID, tstamp, Nsteps=50000, wiggle=1e-3):
+
+    ndim, target, phi, flux, flux_err, qlum, Fth = get_inits(ID, tstamp)
 
     inits = np.array([target.phi_a, target.theta_a, 
                       target.a, target.fwhm, 
@@ -46,8 +55,7 @@ def run_mcmc(ID, tstamp, Nsteps=50000, wiggle=1e-3, nwalkers=32):
     backend = emcee.backends.HDFBackend(filename)
     backend.reset(nwalkers, ndim)
     
-    qlum = target.qlum_erg_s * u.erg / u.s
-    Fth = target.Fth * u.erg / u.s / (u.cm)**2
+    
 
     with Pool(5) as pool:
         sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probs[target.log_prob][1],
@@ -58,26 +66,54 @@ def run_mcmc(ID, tstamp, Nsteps=50000, wiggle=1e-3, nwalkers=32):
                                           "i_sigma":target.i_sigma}),
                                     backend=backend,pool=pool)
         start = time.time()
-        sampler.run_mcmc(pos, Nsteps, progress=True)
+        sampler.run_mcmc(pos, Nsteps, progress=True, store=True)
         end = time.time()
         multi_data_time = end - start
         print("Multiprocessing took {0:.1f} seconds".format(multi_data_time))
-       # print("{0:.1f} times faster(?) than serial".format(serial_data_time / multi_data_time))
-#     sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probs[target.log_prob][1],
-#                                     args=(qlum,#phi, flux, flux_err, 
-#                                           Fth, (target.R_Rsun*R_sun).to("cm"), 
-#                                           target['median'],
-#                                           {"i_mu":target.i_mu,
-#                                           "i_sigma":target.i_sigma}),
-#                                     backend=backend)
-
-#     sampler.run_mcmc(pos, Nsteps, progress=True, store=True);
 
 
+def continue_mcmc(ID, tstamp, Nsteps=50000):
+    
+    ndim, target, phi, flux, flux_err, qlum, Fth = get_inits(ID, tstamp)
+    
+    filename = f"{CWD}/analysis/results/mcmc/{tstamp}_{ID}_MCMC.h5"
+    new_backend = emcee.backends.HDFBackend(filename)
+    print("Initial size: {0}".format(new_backend.iteration))
+    YN = input("Continue? (1/0)")
+    if YN != "1":
+        print("Do not continue.")
+        return
+    elif YN == "1":
+        with Pool(5) as pool:
+            new_sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probs[target.log_prob][1],
+                                        args=(phi, flux, flux_err, qlum,
+                                              Fth, (target.R_Rsun*R_sun).to("cm"), 
+                                              target['median'],
+                                              {"i_mu":target.i_mu,
+                                              "i_sigma":target.i_sigma}),
+                                        backend=new_backend,pool=pool)
+            start = time.time()
+            new_sampler.run_mcmc(None, Nsteps, progress=True, store=True)
+            end = time.time()
+            multi_data_time = end - start
+            print("Final size: {0}".format(new_backend.iteration))
+            print("Multiprocessing took {0:.1f} seconds".format(multi_data_time))
+        
 
 if __name__ == "__main__":
 # Read ID from keyboard here
     ID = '100004076'#input("ID? ")
     tstamp = '04_02_2020_12_01'#input("tstamp? ")
     Nsteps = 10#input("Number of steps? ")
-    run_mcmc(ID, tstamp, Nsteps=int(Nsteps))
+    filename = f"{CWD}/analysis/results/mcmc/{tstamp}_{ID}_MCMC.h5"
+
+    if os.path.isfile(filename):
+        print ("File exist")
+        continue_mcmc(ID, tstamp, Nsteps=int(Nsteps))
+    else:
+        print ("File does not exist")
+        run_mcmc(ID, tstamp, Nsteps=int(Nsteps))
+
+
+    
+    
