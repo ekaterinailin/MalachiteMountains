b695c0fc00dc703368d25b049761aeb4c2f63458
ekaterina <eilin@aip.de>
2020-07-31 12:05:27 +0200

    Tables and plots and script for post analysis.

---
+++ analysis/notebooks/get_mcmc_outputs.py
@@ -0,0 +1,188 @@
+"""
+UTF-8, Python 3
+
+------------------
+MalachiteMountains
+------------------
+
+Ekaterina Ilin, 2020, MIT License
+
+
+This script takes the converted_mcmc_sample.csv
+files from the MCMC fitting, add radius of AR,
+flare ED, flare energy, and write out a new table
+with percentiles for each posterior distribution.
+
+If AR, flare ED, and flare energy were not in the
+read file, add them and save the result to save 
+computation time.
+"""
+
+import pandas as pd
+
+import os
+
+import astropy.units as u
+from astropy.constants import R_sun
+
+from funcs.model import calculate_angular_radius, calculate_ED
+
+import matplotlib 
+matplotlib.rc('xtick', labelsize=14) 
+matplotlib.rc('ytick', labelsize=14) 
+
+font = {'family' : 'courier',
+        'weight' : 'normal',
+        'size'   : 14}
+
+matplotlib.rc('font', **font)
+
+import matplotlib.pyplot as plt
+
+
+style = {'277539431' : ["b","solid"],
+         '100004076' : ["orange",(0, (3, 1, 1, 1, 1, 1))],
+         '44984200' : ["r","dashed"],
+         '237880881a' : ["grey","dotted"],
+         '237880881b' : ["grey","dashdot"]}
+
+def write_mcmc_output(resultframe, tstamp, ID, suffix, CWD):
+    af = resultframe.quantile([.16,0.5,.84], axis=0)
+
+    series = (pd.Series(af.loc[.16,]).
+     rename(lambda x: x+"_16").
+     append(pd.Series(af.loc[.5,]).
+            rename(lambda x: x+"_50")).
+     append(pd.Series(af.loc[.84,]).
+            rename(lambda x: x+"_84")).
+     append(pd.Series({"tstamp":tstamp,
+                       "ID":ID,
+                       "suffix":suffix,
+                       "color":style[str(ID)+suffix][0],
+                       "linestyle":style[str(ID)+suffix][1]})).
+     sort_index(ascending=True))
+
+    with open(f"{CWD}/analysis/results/mcmc/mcmcoutput.csv","a") as f:
+        #Add more lines here
+
+
+        pd.DataFrame(series).T.to_csv(f, index=False)
+
+def write_meta_mcmc(CWD, tstamp, ID, burnin, steps, walkers, nparam, ndim):
+    with open(f"{CWD}/analysis/results/mcmc/mcmc_meta.csv","a") as f:
+        out = f"{tstamp},{ID},{burnin},{steps},{walkers},{nparam},{ndim}"
+        firstout = f"date,ID,burnin,steps,walkers,nparam,ndim"
+        out += "\n"
+        firstout += "\n"
+        f.write(firstout)
+        f.write(out)
+        
+        
+def get_rad_r_and_plot(ID, resultframe, Fth, qlum, R, CWD, tstamp):
+    if "rad_rsun" not in resultframe.columns:
+        g = lambda x: calculate_angular_radius(Fth, x.a, qlum, R)
+        resultframe["rad_rsun"] = resultframe.apply(g, axis=1)
+
+    plt.hist(resultframe.rad_rsun.values, bins=100, 
+             histtype="step", color="k", linewidth=2);
+    plt.xlabel("angular radius [deg]")
+    plt.savefig(f"{CWD}/analysis/plots/mcmc/"
+                f"{tstamp}_{ID}_active_region_radii.png",
+                dpi=300)
+    return resultframe
+
+def get_ED_and_plot(ID, resultframe, lc, CWD, tstamp):
+    
+    if "ED_s" not in resultframe.columns:
+        g = lambda x: calculate_ED(lc.t.values, x.t0_d, x.fwhm_d, x.a)
+        resultframe["ED_s"] = resultframe.apply(g, axis=1)
+
+    plt.hist(resultframe.ED_s.values, bins=200, 
+         histtype="step", color="k", linewidth=2);
+    plt.xlabel("ED [s]")
+    plt.savefig(f"{CWD}/analysis/plots/mcmc/"
+                f"{tstamp}_{ID}_flare_ED.png",
+                dpi=300)
+    return resultframe
+    
+
+def get_E_and_plot(ID, resultframe, lc, qlum, CWD, tstamp):
+    
+    if "ED_s" not in resultframe.columns:
+        get_ED_and_plot(resultframe, lc, CWD)
+        
+    if "Eflare_erg" not in resultframe.columns:
+        g = lambda x: x.ED_s * qlum.value
+        resultframe["Eflare_erg"] = resultframe.apply(g, axis=1)
+        
+    plt.hist(resultframe.Eflare_erg.values, bins=200, 
+         histtype="step", color="k", linewidth=2);
+    plt.xlabel(r"$E_f$ [erg]")
+    plt.savefig(f"{CWD}/analysis/plots/mcmc/"
+                f"{tstamp}_{ID}_flare_E.png",
+                dpi=300)
+    return resultframe
+
+
+
+if __name__ == "__main__":
+
+    # Datasets we analysed
+    datasets = [(277539431, "", "08_07_2020_11_48"),
+                (237880881, "a", "11_02_2020_10_07"),
+                (237880881, "b", "11_02_2020_10_07"),
+                ]
+
+    for dataset in range(3):
+        # What dataset do you want to analyse?
+        ID, suffix, tstamp = datasets[dataset]
+
+        # Pick up the input parameters
+        CWD = "/".join(os.getcwd().split("/")[:-2])
+        inits = pd.read_csv(f"{CWD}/data/summary/inits.csv")
+        inits = inits[(inits.ID == str(ID)+suffix) &
+                      (inits.tstamp==tstamp)].iloc[0]
+
+        qlum = inits.qlum_erg_s * u.erg / u.s
+        Fth = inits.Fth * u.erg / u.cm**2 / u.s
+        median = inits['median']
+        R = inits.R_Rsun * R_sun
+
+        # Pick up the LC
+        lc = pd.read_csv(f"{CWD}/data/lcs/{tstamp}_{ID}.csv")
+
+        # Load MCMC chain
+        resultframe = pd.read_csv(f"{CWD}/analysis/results/mcmc/"
+                                  f"{tstamp}_{ID}{suffix}_converted_mcmc_sample.csv")
+        old = resultframe.shape[1]
+
+        if 'Unnamed: 0' in resultframe.columns:
+            resultframe = resultframe.drop('Unnamed: 0', axis=1)
+        print(resultframe.columns)
+
+        # CALCULATE RADIUS OF AR, FLARE ED, and FLARE E
+        # ----------------------------------------------
+
+        # Get radius of AR distribution and plot a histogram
+        resultframe = get_rad_r_and_plot(ID,resultframe, Fth, qlum, R, CWD, tstamp)
+
+        # Get ED distribution and plot a histogram
+        resultframe = get_ED_and_plot(ID, resultframe, lc, CWD, tstamp)
+
+        # Get E distribution and plot a histogram
+        resultframe = get_E_and_plot(ID, resultframe, lc, qlum, CWD, tstamp)
+
+        # CALCULATE PERCENTILES FOR ALL MCMC OUTPUTS AND THE NEW ONES
+        # ----------------------------------------------
+
+        # Write percentiles to table
+        write_mcmc_output(resultframe, tstamp, ID, suffix, CWD)
+
+        # ADD NEW COLUMNS TO MCMC SAMPLE FILE
+        # -----------------------------------------------
+
+        # But only if you added new columns
+        if resultframe.shape[1] > old:
+            path = (f"{CWD}/analysis/results/mcmc/"
+                    f"{tstamp}_{ID}{suffix}_converted_mcmc_sample.csv")
+            resultframe.to_csv(path, index=False)
---
+++ analysis/notebooks/latexify_mcmc_output.py
@@ -0,0 +1,132 @@
+"""
+UTF-8, Python 3
+
+------------------
+MalachiteMountains
+------------------
+
+Ekaterina Ilin, 2020, MIT License
+
+
+This script converts the mcmcoutput.csv table
+to the latex table that appears in the paper.
+"""
+
+import pandas as pd
+import numpy as np
+import os
+
+def add_val_with_percentiles(df, val, out, suff = ["_16","_50","_84"]):
+    """Convert table with percentiles 
+    to latex compatible table.
+    
+    Parameters:
+    ------------
+    df : pandas table
+        table with three columns per value
+    val : str
+        column name without suffixes
+    out : str
+        formatted column name for latex output
+    suff : list of three strings
+        suffixes to val
+    """
+    
+    for s in suff:
+        df[val+s] = df[val+s].apply(lambda x: float(x))
+    
+    df["vplus"] = df.apply(lambda x: x[val+suff[2]]-x[val+suff[1]], axis=1)
+    df["vminus"] = df.apply(lambda x: x[val+suff[1]]-x[val+suff[0]], axis=1)
+    power = int(np.round(np.log10(np.abs(float(np.min(df.vplus))))))
+    #print(power, "POWER")
+    powerabs = int(np.round(np.log10(np.abs(float(np.min( df[val+suff[1]]))))))
+    #print(powerabs, "POWERABS")
+    if abs(powerabs) > 5:
+        out = out + f"$\cdot 10^{powerabs}"
+        out = out.replace("^","^{")
+        out = out + "}$"
+        df.vminus = df.vminus/(10**(power))
+        df.vplus = df.vplus/(10**(power))
+        df[val+suff[1]] = df[val+suff[1]]/(10**powerabs)
+        
+        df[out] = df.apply(lambda x: f"${x[val+suff[1]]:.{powerabs-power}f}\left(^{x.vplus:.0f}_{x.vminus:.0f}\right)$", axis=1)
+        df[out] = df[out].apply(lambda x: x.replace("^","^{").replace("_","}_{").replace("\right)","}\right)"))
+    else:
+        #print(df.vplus/(10**power))
+        if (df.vplus/(10**power)<1).any():
+            power -= 1
+            #print("TRUE")
+        if powerabs - power < 0:
+            raise ValueError("uncertainty larger than 50th percentile")
+        if power < 0: r = abs(power)
+        elif power >= 0: r = 0
+        df[out] = df.apply(lambda x: f"${x[val+suff[1]]:.{r}f}\left(^{x.vplus/(10**power):.0f}_{x.vminus/(10**power):.0f}\right)$", axis=1)
+        df[out] = df[out].apply(lambda x: x.replace("^","^{").replace("_","}_{").replace("\right)","}\right)"))
+                                        
+    
+    del df['vminus'], df['vplus']
+    for s in suff:
+        del df[val+s]
+    return df
+
+
+
+if __name__ == "__main__":
+    # Get results table that you want to convert:
+    CWD = "/".join(os.getcwd().split("/")[:-2])
+    df = pd.read_csv(f"{CWD}/analysis/results/mcmc/mcmcoutput.csv")
+    df = df.drop_duplicates(keep=False).fillna("")
+
+    # Make sure that FWHM is FWHM as normal people understand it and not 2*FWHM!
+    for col in ['fwhm_d_16', 'fwhm_d_50', 'fwhm_d_84']:
+        df[col] = df[col].astype(float) / 2.
+
+
+    # What values do you want to convert and how to call them                                
+    valout = [("t0_d", "$t_0$ (BJD)"),
+              ("Eflare_erg","$E_{f}$ (erg)"),
+              ("ED_s", "$ED$ (s)"),
+              ("rad_rsun", "$\omega/2$ (deg)"),
+              ("phase_deg","$\phi_0$ (deg)"),
+              ("a","$a$"),
+              ("i_deg","$i$ (deg)"),
+              ("fwhm_d", "FWHM (d)"),
+              ("latitude_deg", "$\theta_f$ (deg)")]
+
+    # Do the conversion
+    cp = df.copy(deep=True)
+    for val, out in valout:
+        print("Convert to LaTex: ", val)
+        cp = add_val_with_percentiles(df, val, out)
+
+    # Merge ID and suffix
+    cp["ID"] = cp.ID + cp.suffix                               
+
+
+    # Remove helper columns
+    del cp['tstamp']
+    del cp['color']
+    del cp['linestyle']
+    del cp['suffix']
+
+
+    # Convert pandas to latex string
+    df3 = cp.copy()
+
+    nc = 'c' * (df3.shape[1]-2) #number of middle columns
+
+    footnote_pref= "\multicolumn{" + str(df3.shape[1]-2) + "}{l}" 
+    footnote_suf = "\n"
+
+    stri = df3.to_latex(index=False,escape=False, column_format=f"l{nc}r")
+    stri = stri.replace("\\toprule","\hline\hline")
+    stri = stri.replace("\\midrule","\hline")
+    stri = stri.replace("\\bottomrule","\hline\n" )
+
+
+    # Path to paper table directory
+    PWD = "/home/ekaterina/Documents/002_writing/multiperiod-flares-draft"
+
+    # Write out latext string
+    with open(f"{PWD}/tables/results.tex", "w") as f:
+        f.write(stri)
