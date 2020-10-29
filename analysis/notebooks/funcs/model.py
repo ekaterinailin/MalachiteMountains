c9e78f5e44fd98564ef73762d334437b9d4abccb
Ekaterina Ilin <eilin@aip.de>
2020-10-29 10:52:48 +0100

    Added a decoupled version of Davenport model.

--- analysis/notebooks/funcs/model.py
+++ analysis/notebooks/funcs/model.py
@@ -704,7 +704,77 @@ def aflare(t, tpeak, dur, ampl, upsample=False, uptime=10):
 
     return flare
 
+def aflare_decoupled(t, tpeak, dur, ampl, upsample=False, uptime=10):
+    '''
+    The Analytic Flare Model evaluated for a single-peak (classical).
+    Reference Davenport et al. (2014) http://arxiv.org/abs/1411.3723
+    Use this function for fitting classical flares with most curve_fit
+    tools.
+    Note: this model assumes the flux before the flare is zero centered
+    Parameters
+    ----------
+    t : 1-d array
+        The time array to evaluate the flare over
+    tpeak : float
+        The time of the flare peak
+    dur : float, float
+        Rise and decay fwhm of the flare
+    ampl : float
+        The amplitude of the flare
+    upsample : bool
+        If True up-sample the model flare to ensure more precise energies.
+    uptime : float
+        How many times to up-sample the data (Default is 10)
+    Returns
+    -------
+    flare : 1-d array
+        The flux of the flare model evaluated at each time
+    '''
+    _fr = [1.00000, 1.94053, -0.175084, -2.24588, -1.12498]
+    _fd = [0.689008, -1.60053, 0.302963, -0.278318]
 
+    fwhm1, fwhm2 = dur # crude approximation for a triangle shape would be dur/2.
+
+    if upsample:
+        dt = np.nanmedian(np.diff(t))
+        timeup = np.linspace(min(t)-dt, max(t)+dt, t.size * uptime)
+
+        flareup = np.piecewise(timeup, [(timeup<= tpeak) * (timeup-tpeak)/fwhm1 > -1.,
+                                        (timeup > tpeak)],
+                                    [lambda x: (_fr[0]+                       # 0th order
+                                                _fr[1]*((x-tpeak)/fwhm1)+      # 1st order
+                                                _fr[2]*((x-tpeak)/fwhm1)**2.+  # 2nd order
+                                                _fr[3]*((x-tpeak)/fwhm1)**3.+  # 3rd order
+                                                _fr[4]*((x-tpeak)/fwhm1)**4. ),# 4th order
+                                     lambda x: (_fd[0]*np.exp( ((x-tpeak)/fwhm2)*_fd[1] ) +
+                                                _fd[2]*np.exp( ((x-tpeak)/fwhm2)*_fd[3] ))]
+                                    ) * np.abs(ampl) # amplitude
+
+        # and now downsample back to the original time...
+        ## this way might be better, but makes assumption of uniform time bins
+        # flare = np.nanmean(flareup.reshape(-1, uptime), axis=1)
+
+        ## This way does linear interp. back to any input time grid
+        # flare = np.interp(t, timeup, flareup)
+
+        ## this was uses "binned statistic"
+        downbins = np.concatenate((t-dt/2.,[max(t)+dt/2.]))
+        flare,_,_ = binned_statistic(timeup, flareup, statistic='mean',
+                                     bins=downbins)
+
+    else:
+        flare = np.piecewise(t, [(t<= tpeak) * (t-tpeak)/fwhm1 > -1.,
+                                 (t > tpeak)],
+                                [lambda x: (_fr[0]+                       # 0th order
+                                            _fr[1]*((x-tpeak)/fwhm1)+      # 1st order
+                                            _fr[2]*((x-tpeak)/fwhm1)**2.+  # 2nd order
+                                            _fr[3]*((x-tpeak)/fwhm1)**3.+  # 3rd order
+                                            _fr[4]*((x-tpeak)/fwhm1)**4. ),# 4th order
+                                 lambda x: (_fd[0]*np.exp( ((x-tpeak)/fwhm2)*_fd[1] ) +
+                                            _fd[2]*np.exp( ((x-tpeak)/fwhm2)*_fd[3] ))]
+                                ) * np.abs(ampl) # amplitude
+
+    return flare
 
 def calculate_ED(t, t0, dur, ampl):
     """Calculate equiavlent duration
