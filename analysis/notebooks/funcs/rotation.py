43de7fa92de50db9e361dd95153e20fefbc2fc78
Ekaterina Ilin <eilin@aip.de>
2020-12-31 05:46:42 +0100

    Added a script that works with the new FlareModulator class, mergend multiperiod.py into rotation.py.

--- analysis/notebooks/funcs/rotation.py
+++ analysis/notebooks/funcs/rotation.py
@@ -9,10 +9,7 @@ Ekaterina Ilin, 2020, MIT License
 
 
 This module contains functions used to 
-calculte absolute K magnitude and errors
-from Gaia distances, and transform them
-to stellar radii using Mann et al. (2016)
-relations.
+calculate and fit rotation periods.
 """
 
 
@@ -20,19 +17,93 @@ relations.
 import pandas as pd
 import numpy as np
 from scipy import optimize
+import astropy.units as u
 
 # matplotlib
 import matplotlib.pyplot as plt
 
 # funcs
-from .helper import read_custom_aperture_lc, fetch_lightcurve
-from .multiperiod import find_period
+from .helper import fetch_lightcurve
 
 # mcmc
 import emcee
 import time as Timestamp
 
-    
+# data management    
+import os
+import copy
+
+
+
+
+
+
+CWD = os.getcwd()
+
+
+
+def find_period(target, minfreq=1, maxfreq=15, plot=True, 
+                save=True, path=CWD,
+                custom=True, flc=None):
+    """Find dominant periodic modulation using
+    Lomb-Scargle periodogram.
+
+    Parameters:
+    -----------
+    target : Series
+        Description of the target.
+    minfreq : float
+        minimum modulation period in 1/day
+    maxfreq : float
+        maximum modulation period in 1/day
+    plot : bool
+        If True, will plot the periodogram
+    save : bool
+        If True, will save periodogram plot to file.
+    path : str
+        Path to file
+    custom : True
+        use custom extracted LCs
+    flc : FlareLightCurve
+        if custom is False, use this LC
+        
+    Return:
+    -------
+    period, frequency : astropy Quantities
+        dominant modulation period in hours,
+        and frequency in 1/day, respectively.
+    """
+        # Fetch light curve
+    if custom==True:
+        flck = fetch_lightcurve(target)
+    else: 
+        flck = flc
+   
+    # Use Lomb-Scargle periodogram implemented in lightkurve
+    pg = flck.remove_nans().to_periodogram(freq_unit=1/u.d,
+                                              maximum_frequency=maxfreq,
+                                              minimum_frequency=minfreq)
+
+    # Convert frequency to period
+    period = (1 / pg.frequency_at_max_power).to("h")
+
+    if plot==True:
+
+        # Plot the periodogram
+        pg.plot(label=f"{target.prefix} {target.ID}, S{target.QCS}, {target.SpT}V \n {period:.2f}")
+        plt.xlim(minfreq, maxfreq)
+
+        # Optionally save to file
+        if save==True:
+            plt.savefig(f"{path}/{target.ID}_{target.QCS}_periodogram.png",dpi=300)
+
+    # Stdout
+    print(f"{target.prefix} {target.ID} modulation period: ", period)
+
+    return period, pg.frequency_at_max_power
+
+
+
 def rotation_period_uncertainties_mcmc(ID, sector, target, res, step=50, 
                                        maxiterations=20, CWD="", tstamp=""):
     """
--- analysis/notebooks/funcs/tests/test_rotation.py
+++ analysis/notebooks/funcs/tests/test_rotation.py
@@ -7,26 +7,11 @@ import os
 
 from altaipony.flarelc import FlareLightCurve
 
-from ..multiperiod import find_period
+from ..rotation import find_period
 
 CWD = os.getcwd()
 
 def test_find_period():
-    
-    def create_lc(hours):
-        # Create a light curve with a sinusoidal modulation
-        hdr = fits.Header()
-        hdr['OBSERVER'] = 'Mike'
-        hdr['COMMENT'] = "Here's some commentary about this FITS file."
-        start, stop, N = 1000, 1020, 10000
-        c1 = fits.Column(name='TIME', array=np.linspace(start, stop, N), format='F10.4')
-        c2 = fits.Column(name='FLUX', array=400 + 50*np.sin(np.linspace(start, stop, N)*np.pi*48/hours), format='F10.4')
-        c3 = fits.Column(name='FLUX_ERR', array= 20*np.random.rand(N), format='F10.4')
-        c4 = fits.Column(name='QUALITY', array= np.full(N,0), format='K')
-        c5 = fits.Column(name='CADENCENO', array= np.arange(100,N+100), format='K')
-        hdu = fits.BinTableHDU.from_columns([c1,c2,c3,c4,c5], header=hdr)
-        PATH = f"{CWD}/1000_10_tess_custom_flc.fits"
-        hdu.writeto(PATH,overwrite=True)
 
     # Create a target description
     target = pd.Series({"h_mission": "TESS",
